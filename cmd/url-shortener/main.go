b86f0013cc545f401f634e071b8d075ad4d5549f
grSashha <90776548+grSashha@users.noreply.github.com>
2024-12-29 22:19:57 +0300

    setting logs

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/al-grigorian/url-shortener/internal/config"
 )
 
+const envLocal = "local"
+const envDev = "dev"
+const envProd = "prod"
+
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("startig url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init logger: slog
 
@@ -17,4 +28,25 @@ func main() {
 	// TODO: init router: "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
